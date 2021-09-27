410e15a8e093e9a48063edec46add45940a13963
Stefan Maron <stefan.maron@orbit.de>
2021-09-27 08:30:31 +0200

    Some bugfixes in the powershell script that prevented download

--- DownloadFile.ps1
+++ DownloadFile.ps1
@@ -1,16 +1,22 @@
 param (
     $TargetPath
 )
+$ErrorActionPreference = 'Stop'
 
-$WebClient = New-Object System.Net.WebClient
 $latestRelease = Invoke-RestMethod -Uri "https://api.github.com/repos/StefanMaron/BusinessCentral.LinterCop/releases/latest"
 $latestRelease.assets[0].browser_download_url
-$lastVersionTimeStamp = Get-Content -Path (Join-Path $PSScriptRoot 'lastversion.txt')
+$lastVersionTimeStamp = ''
+$lastVersionTimeStamp = Get-Content -Path (Join-Path $PSScriptRoot 'lastversion.txt') -ErrorAction SilentlyContinue
 
-if ((($lastVersionTimeStamp -ne '') -and ((Get-Date $lastVersionTimeStamp) -lt (Get-Date $latestRelease.published_at ))) -or (-not (Test-Path $TargetPath -PathType leaf))) {
+if ($lastVersionTimeStamp -ne '') {
+$lastVersionTimeStamp = '0001-01-01T00:00:00Z'
+
+}
+
+if (((Get-Date $lastVersionTimeStamp) -lt (Get-Date $latestRelease.published_at )) -or (-not (Test-Path $TargetPath -PathType leaf))) {
+    Remove-Item -Path $TargetPath -Force
+    Invoke-WebRequest -Uri $latestRelease.assets[0].browser_download_url -OutFile $TargetPath
     Set-Content -Value $latestRelease.published_at -Path (Join-Path $PSScriptRoot 'lastversion.txt')
-    
-    $WebClient.DownloadFile($latestRelease.assets[0].browser_download_url, $TargetPath)
     return 1
 }
 
